6b6a8bd678beb4ab5a6e5cd93a4339b6716788ab
Andrey Zubov <taac@mail.ru>
2025-11-30 19:23:05 +0400

    Save data to local

---
+++ app/datahandler.py
@@ -0,0 +1,22 @@
+import os
+
+
+from typing import Union, Any
+
+
+class StorageData:
+    def __init__(self):
+        self.storage = os.getenv('STORAGE')
+
+        if self.storage == 's3':
+            pass  # todo add s3 init with boto
+
+    def save_data(self, data: Union[str, dict[str, Any]]):
+        if isinstance(data, dict):
+            data = str(data)
+
+        if self.storage == 'local':
+            with open('data/saved_data', 'w') as file:
+                file.write(data + '\n')
+        elif self.storage == 's3':
+            pass
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,13 @@
 from typing import Any
 
+from app.datahandler import StorageData
+
+
+storage = StorageData()
+
+
 def postprocess_data(data: dict[str, Any]):
+    storage.save_data(data)
     keys_to_keep = ['name', 'main', 'wind', 'clouds']
     cleaned_data = {k: data.get(k) for k in keys_to_keep}
     return cleaned_data
